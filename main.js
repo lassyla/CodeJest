9325ac20f48b2dcc26e6269af6abd2ed323e8155
lassyla <a.alyssa.lee@gmail.com>
2017-10-28 11:27:33 -0400

    added text inputs

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 var images = document.getElementById("images");
 var text = document.getElementById("text");
+var hintText = document.getElementById("hinttext"); 
 var buttonBox = document.getElementById('buttonBox');
-var input = document.getElementById('input');
+var textbox = document.getElementById('textbox');
 var yerdog;
 
-input.onkeypress = function(event) {
-  if (event.key == "Enter" || event.keyCode == 13) {
-    yerdog =  input.value;
-    input.parentNode.removeChild(input)
-    advanceTo(scenario.two)
+textbox.onkeypress = function(event) {
+    if (event.key == "Enter" || event.keyCode == 13) {
+        parseInput(textbox.value, scenario.one);
+        textbox.value = ""; 
   }
 };
 
 
 var changeText = function(words) {
-  text.innerHTML = words.replace("Your dog", yerdog);
+    text.innerHTML = words.replace("Your dog", yerdog);
+    hinttext.innerHTML = ""; 
+};
+
+var changeHintText = function(words) {
+  hinttext.innerHTML = words.replace("Your dog", yerdog);
 };
 
 var changeImage = function(img) {
@@ -31,45 +36,42 @@ var changeButtons = function(buttonList) {
 var advanceTo = function(s) {
   changeImage(s.image)
   changeText(s.text)
-  changeButtons(s.buttons)
 };
 
+
+var listFunctionNames = function(s){
+    changeHintText(s.helpText); 
+}
+
+var parseInput = function(input, s){
+    //var inputs = split[input]; 
+    for(var i = 0; i < s.functionNames.length; i++)
+        if(input == s.functionNames[i][0]) 
+        {
+            eval(s.functionNames[i][1]); 
+            return; 
+        }
+    changeHintText("i don't know what that is.");
+}
 
 scenario = {}
 var scenario = {
   one: {
     image: "https://s9.postimg.org/eceo9mp73/5860028206_d66810105f_b.jpg", //dog
-    text: "You have finally decided to take your dog out for a walk. You smile and pat your trusty companion's head. What the dog's name?\n",
+    text: "You are in a room. what to do?\n",
+    helpText: "commands: help, examine, look", 
+    functionNames: [["help", "listFunctionNames(scenario.one)"], ["examine", "advanceTo(scenario.two)"], ["look", "advanceTo(scenario.three)"]]
   },
   two: {
     image: "https://s9.postimg.org/9p8m7v1u7/6899639786_d517c4cce3_z.jpg", //house
-    text: "Your dog yanks at the leash. You hear dogs barking and see an old abandoned house. Strangely, the door is wide open. What do you want to do?",
-    buttons: [["Turn and run", "advanceTo(scenario.three)"],["Enter The House", "advanceTo(scenario.four)"]]
+    text: "you examined something. what now?",
+    functionNames: [["help", "listFunctionNames(scenario.one)"], ["examine", "advanceTo(scenario.two)"], ["look", "advanceTo(scenario.three)"]]
   },
   three: {
     image: "https://s4.postimg.org/t1g20apst/261819008_d4316c1bdf_o.jpg",
-    text: "A wild gang of rabid dogs are gaining on you. Against your better judgement you enter the creepy house for saftey. Your dog is whincing and may be injured.",
-    buttons: [["continue", "advanceTo(scenario.four)"]]
-  },
-  four: {
-    image: "https://s6.postimg.org/kz5m1cnkh/2919478782_c343d14be6_b.jpg",
-    text: "Your dog has run, barking loudly, into the basement. You wonder what's down there. The door jammed when you slammed it behind you. You are going to need something to pry it back open",
-    buttons: [["Follow your Dog Downstairs", "advanceTo(scenario.five)"],["Search the Kitchen for a knife", "advanceTo(scenario.five)"]]
-  },
-  five: {
-    image: "https://s6.postimg.org/kz5m1cnkh/2919478782_c343d14be6_b.jpg",
-    text: "TO BE CONTINUED...",
-    buttons: [["Follow your Dog Downstairs", "advanceTo(scenario.five)"],["Search the Kitchen for a knife", "advanceTo(scenario.five)"]]
-  },
-  six: {
-    image: "",
-    text: "",
-    buttons: [["Follow your Dog Downstairs", "advanceTo(scenario.five)"],["Search the Kitchen for a knife", "advanceTo(scenario.five)"]]
-  },
-  seven: {
-    image: "",
-    text: "",
-    buttons: [["Follow your Dog Downstairs", "advanceTo(scenario.five)"],["Search the Kitchen for a knife", "advanceTo(scenario.five)"]]
+    text: "You looked at something. what now?",
+    functionNames: [["help", "listFunctionNames(scenario.one)"], ["examine", "advanceTo(scenario.two)"], ["look", "advanceTo(scenario.three)"]]
+
   }
   
 };
